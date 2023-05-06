bd3572900a238abf93e54a933912b0f827ea2b22
Kannu Mandora <mandorakannu@gmail.com>
2023-05-06 18:06:13 +0530

    Update Every Dependencies in next-app and we create featurePost and BlogFooter which we want to visible on every page.

--- app/(common)/FeaturedPost/page.tsx
+++ app/(common)/FeaturedPost/page.tsx
@@ -2,6 +2,7 @@ import data from "@jsonsFiles/featurePost.json";
 import image from "@images/featured-post.webp"
 import Image from "next/image";
 import { Outfit } from "next/font/google";
+import BlogFooter from "@components/BlogFooter";
 const outfit = Outfit({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
@@ -18,7 +19,7 @@ function FeaturedPost() {
     <>
       <div className={`grid grid-cols-1 gap-6 justify-start border border-black mx-5 my-10 p-20 ${outfit.className}`}>
         <section className="flex justify-between items-center">
-          <span>May 6 · 2 min read</span>
+          <span>May 6&nbsp;·&nbsp;2 min read</span>
           <span className="scale-150">· · ·</span>
         </section>
         <section className="flex flex-col gap-8 w-4/5 mx-auto my-8">
@@ -26,13 +27,19 @@ function FeaturedPost() {
             return (
               <>
                 <h1 className="text-5xl">{item.title}</h1>
-                <h2 className="text-2xl text-zinc-400">{item.subTitle}</h2>
+                <h2 className="text-2xl font-normal text-zinc-400">{item.subTitle}</h2>
                 <Image
                   src={image}
                   alt="Featured Post"
                 />
                 <h3 className="text-2xl">Author: {item.author}</h3>
-                <p className="">Welcome to your blog post. Use this space to connect with your readers and potential customers in a way that’s current and interesting. Think of it as an ongoing conversation where you can share updates about business, trends, news, and more. </p>
+                <p className="text-lg">Welcome to your blog post. Use this space to connect with your readers and potential customers in a way that’s current and interesting. Think of it as an ongoing conversation where you can share updates about business, trends, news, and more. </p>
+                <section className="flex flex-col justify-center items-start gap-8">
+                  <h1 className="font-bold text-xl">Design with Ease</h1>
+                  <p className="text-lg bg-yellow-100 p-4 border-2 border-red-100">{item.quote}</p>
+                  <p className="text-lg">Every layout comes with the latest social features built in. Readers will be able to easily share posts on social networks like Facebook and Twitter, view how many people have liked a post, made comments and more. With Wix, building your online community has never been easier.</p>
+                </section>
+                <BlogFooter />
               </>
             );
           })}
---
+++ components/BlogFooter.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SocialIcons from "./Social-Icons";
+import Link from "next/link";
+
+export default function BlogFooter() {
+  return (
+    <>
+      <hr />
+      <section className="flex justify-between items-center">
+        <SocialIcons />
+        <Link href="/" className="text-lg text-red-400">
+          Back to Home
+        </Link>
+      </section>
+    </>
+  );
+}
