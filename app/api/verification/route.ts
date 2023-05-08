d1453f6c74cba4fddebb2df06c468e4c377427e7
Kannu Mandora <mandorakannu@gmail.com>
2023-05-08 14:32:29 +0530

    Fix: HEAD to incoming changes & add pathname() for easy writing path in Link tag

--- app/api/verification/route.ts
+++ app/api/verification/route.ts
@@ -1,7 +1,6 @@
 import bcryptjs from "bcryptjs";
 import { cookies } from "next/headers";
 import { sign } from "jsonwebtoken";
-<<<<<<< HEAD
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) : Promise<Response> {
@@ -12,29 +11,16 @@ export async function POST(request: NextRequest) : Promise<Response> {
     process.env.EMAIL_ID!
   );
   const validEmailPassword: boolean = bcryptjs.compareSync(
-=======
-
-export async function POST(request: Request) {
-  const { email, password } = await request.json(); // Accepting the email & password from the client side
-
-  const validEmailId = bcryptjs.compareSync(email, process.env.EMAIL_ID!);
-  const validEmailPassword = bcryptjs.compareSync(
->>>>>>> 13e5a1a8dc84d598ceeb5f3e92336b08d8b18e2d
     password,
     process.env.EMAIL_PASSWORD!
   );
 
-<<<<<<< HEAD
   const headers: Object = {
     headers: { "content-type": "application/json" },
   };
 
   if (validEmailId && validEmailPassword) {
     const token:string = createToken();
-=======
-  if (validEmailId && validEmailPassword) {
-    const token = createToken();
->>>>>>> 13e5a1a8dc84d598ceeb5f3e92336b08d8b18e2d
     // @ts-ignore
     // I am adding this @ts-ignore because I am getting an error in the cookies().set() method, at the time of writing this code. Date: 07-05-2023
     cookies().set({
@@ -45,7 +31,6 @@ export async function POST(request: Request) {
       secure: true,
       sameSite: "strict",
     });
-<<<<<<< HEAD
     return new Response(JSON.stringify({ message: "success" }), headers);
   }
   return new Response(JSON.stringify({ message: "Error" }), headers);
@@ -54,21 +39,6 @@ export async function POST(request: Request) {
 function createToken() : string {
   return sign(
     { id: "1", content: "Authenatication", value:"true" },
-=======
-    return new Response(JSON.stringify({ message: "success" }), {
-      headers: { "content-type": "application/json" },
-    });
-  } else {
-    return new Response(JSON.stringify({ message: "Error" }), {
-      headers: { "content-type": "application/json" },
-    });
-  }
-}
-
-function createToken() {
-  return sign(
-    { id: "1", content: "This is my cookie" },
->>>>>>> 13e5a1a8dc84d598ceeb5f3e92336b08d8b18e2d
     process.env.SECRET_KEY!
   );
 }
--- app/main/dashboard/page.tsx
+++ app/main/dashboard/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import authUser from "@functions/validate";
 import Link from "next/link";
 export default async function Dashboard(): Promise<JSX.Element> {
   const router = useRouter();
+  const path = usePathname();
   const [user, setUser] = useState<boolean>(false);
   useEffect(() => {
     const verifyUser = async () => {
@@ -26,8 +27,8 @@ export default async function Dashboard(): Promise<JSX.Element> {
             Dashboard
           </h1>
           <div className="flex justify-center items-center gap-10 py-10">
-          <Link href="/Create-Blogs" className="bg-red-400 px-6 py-2 text-white rounded" >Create Blogs</Link>
-          <Link href="/Manage-Blogs" className="bg-red-400 px-6 py-2 text-white rounded" >Manage Blogs</Link>
+          <Link href={`${path}/Create-Blogs`} className="bg-red-400 px-6 py-2 text-white rounded" >Create Blogs</Link>
+          <Link href={`${path}/Manage-Blogs`} className="bg-red-400 px-6 py-2 text-white rounded" >Manage Blogs</Link>
           </div>
         </div>
       )}
