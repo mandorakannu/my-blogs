0d7ab9675e8b86b8e841ea4dc305470e1fd05c46
Kannu Mandora <mandorakannu@gmail.com>
2023-05-13 14:58:23 +0530

    Add no-cache properties in /Blogs and [_id]/page.tsx

--- app/(common)/Blogs/[_id]/page.tsx
+++ app/(common)/Blogs/[_id]/page.tsx
@@ -19,7 +19,7 @@ const BlogPage = async (_id: string): Promise<any> => {
   const BASE_URL = process.env.BASE_URL;
   try {
     const res = await fetch(`${BASE_URL}/api/searchBlog`, {
-      next: {revalidate: 30},
+      cache: "no-cache",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
