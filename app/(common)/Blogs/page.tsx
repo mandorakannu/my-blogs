cd602584e55a848a5fce2ac538ace7976b2a1ef2
Kannu Mandora <mandorakannu@gmail.com>
2023-05-19 09:33:35 +0530

    Fix: Blogs is fetch without .map function error

--- app/(common)/Blogs/page.tsx
+++ app/(common)/Blogs/page.tsx
@@ -1,3 +1,5 @@
+import { connectDB } from "@databases/connectionDB";
+import posts from "@models/posts/posts";
 import { Outfit } from "next/font/google";
 import Link from "next/link";
 const outfit = Outfit({
@@ -5,7 +7,7 @@ const outfit = Outfit({
   weight: ["400", "500", "700"],
 });
 
-type Blogs = {
+type BlogsProps = {
   _id: string;
   title: string;
   subTitle: string;
@@ -19,42 +21,40 @@ type Blogs = {
   createdAt: string;
 }[];
 
-const getAllBlogs = async (): Promise<any> => {
+const getAllBlogs = async () => {
   try {
-    const VERCEL_URL = process.env.VERCEL_URL;
-    const res = await fetch(`https://${VERCEL_URL}/api/findBlogs`,{
-      cache: "no-cache",
-      next: {revalidate: 10}
-    });
-    const data = await res.json();
-    return data;
-  } catch (error: unknown) {
+    await connectDB();
+    const blogs = await posts.find({});
+    return blogs;
+  } catch (error) {
     console.log(error);
+    return [];
   }
 };
 
 async function Blogs(): Promise<JSX.Element> {
-  const blogs: Blogs = await getAllBlogs();
+  const posts: BlogsProps = await getAllBlogs();
+  const blogs: BlogsProps = JSON.parse(JSON.stringify(posts));
   return (
     <>
       <section
         className={`grid max-sm:grid-cols-1 md: grid-cols-2 md:grid-cols-3 gap-6 m-6 cursor-default ${outfit.className}`}
       >
-        {blogs.map((blog) => (
+        {blogs.map((post) => (
           <div
-            key={blog._id}
+            key={post._id}
             className="flex flex-col justify-between p-4 bg-white rounded-lg shadow-lg border"
           >
-              <div className="flex flex-col justify-start gap-2">
-                <h1 className="text-xl font-bold">{blog.title}</h1>
-                <h2>{blog.subTitle}</h2>
-              </div>
-              <Link
-                href={`/Blogs/${blog._id}`}
-                className="text-red-500 my-3 w-fit hover:underline underline-offset-4"
-              >
-                Read More...
-              </Link>
+            <div className="flex flex-col justify-start gap-2">
+              <h1 className="text-xl font-bold">{post.title}</h1>
+              <h2>{post.subTitle}</h2>
+            </div>
+            <Link
+              href={`/Blogs/${post._id}`}
+              className="text-red-500 my-3 w-fit hover:underline underline-offset-4"
+            >
+              Read More...
+            </Link>
           </div>
         ))}
       </section>
