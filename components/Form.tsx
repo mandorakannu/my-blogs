88477f35365bb307fafacee41365a83723276c26
Kannu Mandora <mandorakannu@gmail.com>
2023-05-20 21:24:33 +0530

    Remove: Dark Mode from Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -31,9 +31,9 @@ export default function Form() {
   };
   return (
     <>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center">
             Contact Us
           </h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
@@ -44,7 +44,7 @@ export default function Form() {
             <div>
               <label
                 htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="block mb-2 text-sm font-medium"
               >
                 Your email
               </label>
@@ -52,7 +52,7 @@ export default function Form() {
                 type="email"
                 id="email"
                 ref={emailRef}
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
                 placeholder="name@example.com"
                 required
               />
@@ -60,14 +60,14 @@ export default function Form() {
             <div className="sm:col-span-2">
               <label
                 htmlFor="message"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                className="block mb-2 text-sm font-medium"
               >
                 Your message
               </label>
               <textarea
                 id="message"
                 ref={messageRef}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300"
+                className="block p-2.5 w-full text-sm bg-gray-50 rounded-lg shadow-sm border border-gray-300"
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
