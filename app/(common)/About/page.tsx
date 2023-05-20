4555c282f5a99f8e511bf1faac1670207a8417b4
Kannu Mandora <mandorakannu@gmail.com>
2023-05-20 21:19:19 +0530

    Remove: lorem epsum text to real text in about section

--- app/(common)/About/page.tsx
+++ app/(common)/About/page.tsx
@@ -21,35 +21,68 @@ export default function page() {
         <h1 className="text-5xl my-10 text-center">
           Hey! So Glad You&apos;re Here.
         </h1>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 md:px-40">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            veritatis necessitatibus culpa voluptatem sapiente porro,
-            perferendis reprehenderit impedit itaque ab. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Quibusdam obcaecati eum maxime.
-            Illo molestias vitae consequuntur temporibus explicabo deleniti.
-            Error fuga maxime fugiat corrupti at non labore quaerat numquam
-            placeat. Fugiat in libero vitae autem quasi amet laudantium magni
-            voluptatibus vero minus voluptate sed nemo officia necessitatibus
-            aliquam, quas maxime eum ipsam a harum atque officiis facilis. Amet
-            ipsam quas esse consequatur ullam, inventore ipsa quam cum itaque
-            corrupti, qui facilis magnam voluptatum sapiente dolores quo
-            reiciendis quibusdam cupiditate libero?
+            Welcome to my blog! I&apos;m Kannu Mandora, a passionate MERN stack
+            web developer and full-stack developer. I&apos;m thrilled to share
+            my knowledge and experiences in the world of web development with
+            you through this platform.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            veritatis necessitatibus culpa voluptatem sapiente porro,
-            perferendis reprehenderit impedit itaque ab. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Quibusdam obcaecati eum maxime.
-            Illo molestias vitae consequuntur temporibus explicabo deleniti.
-            Error fuga maxime fugiat corrupti at non labore quaerat numquam
-            placeat. Fugiat in libero vitae autem quasi amet laudantium magni
-            voluptatibus vero minus voluptate sed nemo officia necessitatibus
-            aliquam, quas maxime eum ipsam a harum atque officiis facilis. Amet
-            ipsam quas esse consequatur ullam, inventore ipsa quam cum itaque
-            corrupti, qui facilis magnam voluptatum sapiente dolores quo
-            reiciendis quibusdam cupiditate libero?
+            With a deep love for technology and a curiosity for solving complex
+            problems, I have dedicated myself to mastering the MERN stack. The
+            MERN stack is a powerful combination of technologies comprising
+            MongoDB, Express.js, React.js, and Node.js. This comprehensive stack
+            allows me to build robust, scalable, and efficient web applications
+            from the front end to the back end.
           </p>
+          <p>
+            My journey in web development began 1.5 years ago, driven by a
+            strong desire to create innovative and user-friendly digital
+            solutions. I have honed my skills in HTML, CSS, and JavaScript,
+            which form the foundation of any web application. However, my true
+            passion lies in leveraging the capabilities of the MERN stack to
+            develop dynamic and interactive websites.
+          </p>
+          <p>
+            As a full-stack developer, I have experience in both front-end and
+            back-end development. On the front end, I excel in building
+            responsive user interfaces using modern frameworks such as React.js.
+            I strive to create intuitive and visually appealing designs that
+            provide an exceptional user experience. Additionally, I am
+            well-versed in JavaScript libraries like Redux, which enable
+            efficient state management in complex applications.
+          </p>
+          <p>
+            On the back end, I harness the power of Node.js and Express.js to
+            create robust server-side solutions. I specialize in building
+            RESTful APIs that facilitate seamless communication between the
+            front end and the server. With a strong understanding of database
+            management, I utilize MongoDB to store and retrieve data
+            efficiently.
+          </p>
+          <p>
+            Throughout my career, I have had the privilege of working on various
+            projects, ranging from e-commerce platforms to social media
+            applications. These experiences have equipped me with a diverse
+            skill set and an ability to tackle challenges head-on. I am a firm
+            believer in continuous learning and stay updated with the latest
+            industry trends and best practices to deliver cutting-edge
+            solutions.
+          </p>
+          <p>
+            Through this blog, I aim to provide valuable insights, tutorials,
+            and tips related to MERN stack development and the world of web
+            development at large. Whether you are a beginner looking to start
+            your journey in web development or an experienced developer seeking
+            to enhance your skills, this blog will serve as a valuable resource.
+          </p>
+          <p>
+            Thank you for joining me on this exciting adventure. Let&apos;s
+            embark on a journey of exploration and growth in the dynamic field
+            of web development together!
+          </p>
+          <p>Happy coding, Kannu Mandora</p>
         </div>
       </div>
       <Form />
